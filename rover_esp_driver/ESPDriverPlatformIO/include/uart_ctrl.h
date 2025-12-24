e174e2ef69f463158a0b749ffbcbd8d4a0160d36
Eyas Taifour <etaifour@icloud.com>
2025-12-24 13:02:16 +1100

    new driver functional, sends data in JSON

--- rover_esp_driver/ESPDriverPlatformIO/include/uart_ctrl.h
+++ rover_esp_driver/ESPDriverPlatformIO/include/uart_ctrl.h
@@ -1,111 +1,110 @@
-// IMU streaming state (0=off, 1=on)
+// =============================================================================
+// UART Control - Serial Command Processing
+// =============================================================================
+
+#ifndef UART_CTRL_H
+#define UART_CTRL_H
+
+// -----------------------------------------------------------------------------
+// State Variables
+// -----------------------------------------------------------------------------
+
 bool imu_stream_enabled = false;
 
-void jsonCmdReceiveHandler(){
-	int cmdType = jsonCmdReceive["T"].as<int>();
-	switch(cmdType){
-	case CMD_SPEED_CTRL:		if (jsonCmdReceive.containsKey("T") &&
-										jsonCmdReceive.containsKey("L") &&
-										jsonCmdReceive.containsKey("R")){
-									if (jsonCmdReceive["L"].is<float>() &&
-											jsonCmdReceive["R"].is<float>()){
-										heartbeatStopFlag = false;
-										lastCmdRecvTime = millis();
-										setGoalSpeed(
-										jsonCmdReceive["L"],
-										jsonCmdReceive["R"]);
-									}
-								} break;
-	case CMD_PWM_INPUT:			usePIDCompute = false;
-									heartbeatStopFlag = false;
-									lastCmdRecvTime = millis();
-									leftCtrl(jsonCmdReceive["L"]);
-									rightCtrl(jsonCmdReceive["R"]);
-									break;
+// -----------------------------------------------------------------------------
+// JSON Command Handler
+// -----------------------------------------------------------------------------
 
-	case CMD_SET_MOTOR_PID:		setPID(
-									jsonCmdReceive["P"],
-									jsonCmdReceive["I"],
-									jsonCmdReceive["D"],
-									jsonCmdReceive["L"]);break;
+void jsonCmdReceiveHandler() {
+    int cmdType = jsonCmdReceive["T"].as<int>();
 
-	case CMD_OLED_CTRL:			oledCtrl(jsonCmdReceive["lineNum"],
-									jsonCmdReceive["Text"]);break;
+    switch (cmdType) {
 
-	case CMD_OLED_DEFAULT: 		setOledDefault();break;
-	// case CMD_GET_IMU_DATA:						getIMUData();break;
-	// case CMD_CALI_IMU_STEP:
-	// 											imuCalibration();break;
-	// case CMD_GET_IMU_OFFSET:
-	// 											getIMUOffset();
-	// 											break;
-	// case CMD_SET_IMU_OFFSET:
-	// 											setIMUOffset(
-	// 											jsonCmdReceive["x"],
-	// 											jsonCmdReceive["y"],
-	// 											jsonCmdReceive["z"]);break;
-	// case CMD_BASE_FEEDBACK:
-	// 											baseInfoFeedback();break;
-	// case CMD_BASE_FEEDBACK_FLOW:
-	// 											setBaseInfoFeedbackMode(
-	// 											jsonCmdReceive["cmd"]);break;
-	// case CMD_FEEDBACK_FLOW_INTERVAL:
-	// 											setFeedbackFlowInterval(
-	// 											jsonCmdReceive["cmd"]);break;
-	// case CMD_UART_ECHO_MODE:
-	// 											setCmdEcho(
-	// 											jsonCmdReceive["cmd"]);break;
-	// case CMD_SET_SPD_RATE:
-	// 											setSpdRate(
-	// 											jsonCmdReceive["L"],
-	// 											jsonCmdReceive["R"]);break;
-	// case CMD_GET_SPD_RATE:
-	// 											getSpdRate();break;
-	// case CMD_SAVE_SPD_RATE:
-	// 											saveSpdRate();break;
+    // --- Motor Control ---
 
-	// esp-32 dev ctrl.
-	case CMD_REBOOT: 			esp_restart();break;
+    case CMD_SPEED_CTRL:
+        if (jsonCmdReceive.containsKey("L") && jsonCmdReceive.containsKey("R")) {
+            if (jsonCmdReceive["L"].is<float>() && jsonCmdReceive["R"].is<float>()) {
+                heartbeatStopFlag = false;
+                lastCmdRecvTime = millis();
+                setGoalSpeed(jsonCmdReceive["L"], jsonCmdReceive["R"]);
+            }
+        }
+        break;
 
+    case CMD_PWM_INPUT:
+        usePIDCompute = false;
+        heartbeatStopFlag = false;
+        lastCmdRecvTime = millis();
+        leftCtrl(jsonCmdReceive["L"]);
+        rightCtrl(jsonCmdReceive["R"]);
+        break;
 
-	// mainType & moduleType settings.
-	case CMD_MM_TYPE_SET: mm_settings(
-												jsonCmdReceive["main"],
-												jsonCmdReceive["module"]
-												);
-												break;
+    case CMD_SET_MOTOR_PID:
+        setPID(
+            jsonCmdReceive["P"],
+            jsonCmdReceive["I"],
+            jsonCmdReceive["D"],
+            jsonCmdReceive["L"]
+        );
+        break;
 
-	// IMU streaming control
-	case CMD_IMU_STREAM_CTRL:
-		if (jsonCmdReceive.containsKey("cmd")) {
-			imu_stream_enabled = (jsonCmdReceive["cmd"].as<int>() == 1);
-		}
-		break;
-	}
+    // --- OLED Display ---
+
+    case CMD_OLED_CTRL:
+        oledCtrl(jsonCmdReceive["lineNum"], jsonCmdReceive["Text"]);
+        break;
+
+    case CMD_OLED_DEFAULT:
+        setOledDefault();
+        break;
+
+    // --- IMU Streaming ---
+
+    case CMD_IMU_STREAM_CTRL:
+        if (jsonCmdReceive.containsKey("cmd")) {
+            imu_stream_enabled = (jsonCmdReceive["cmd"].as<int>() == 1);
+        }
+        break;
+
+    // --- System Control ---
+
+    case CMD_REBOOT:
+        esp_restart();
+        break;
+
+    // --- Platform Configuration ---
+
+    case CMD_MM_TYPE_SET:
+        mm_settings(jsonCmdReceive["main"], jsonCmdReceive["module"]);
+        break;
+    }
 }
 
+// -----------------------------------------------------------------------------
+// Serial Control Loop
+// -----------------------------------------------------------------------------
 
 void serialCtrl() {
-  static String receivedData;
+    static String receivedData;
 
-  while (Serial.available() > 0) {
-    char receivedChar = Serial.read();
-    receivedData += receivedChar;
+    while (Serial.available() > 0) {
+        char receivedChar = Serial.read();
+        receivedData += receivedChar;
 
-    // Detect the end of the JSON string based on a specific termination character
-    if (receivedChar == '\n') {
-      // Now we have received the complete JSON string
-      DeserializationError err = deserializeJson(jsonCmdReceive, receivedData);
-      if (err == DeserializationError::Ok) {
-  			if (InfoPrint == 1 && uartCmdEcho) {
-  				Serial.print(receivedData);
-  			}
-        jsonCmdReceiveHandler();
-      } else {
-        // Handle JSON parsing error here
-      }
-      // Reset the receivedData for the next JSON string
-      receivedData = "";
+        if (receivedChar == '\n') {
+            DeserializationError err = deserializeJson(jsonCmdReceive, receivedData);
+
+            if (err == DeserializationError::Ok) {
+                if (InfoPrint == 1 && uartCmdEcho) {
+                    Serial.print(receivedData);
+                }
+                jsonCmdReceiveHandler();
+            }
+
+            receivedData = "";
+        }
     }
-  }
 }
+
+#endif // UART_CTRL_H
--- rover_esp_driver/ESPDriverPlatformIO/src/main.cpp
+++ rover_esp_driver/ESPDriverPlatformIO/src/main.cpp
@@ -1,144 +1,107 @@
-// Devices found
-// 0C - Must be the compass, AKAK09918
-// 3C - OLED
-// 6B - QMI8658_ADDR - https://www.waveshare.com/w/upload/3/36/QMI8658C_datasheet_rev_0.8.pdf
-// 6B provides temperature, ax, ay, az, Gx, Gy, Gz
-// 42 - INA219: Voltage/Current
+// =============================================================================
+// Waveshare Rover ESP32 Driver
+// =============================================================================
+//
+// I2C Devices:
+//   0x0C - AK09918C Magnetometer
+//   0x3C - SSD1306 OLED Display
+//   0x6B - QMI8658C IMU (Accel + Gyro)
+//   0x42 - INA219 Voltage/Current Sensor
+//
+// =============================================================================
 
+// -----------------------------------------------------------------------------
+// Libraries
+// -----------------------------------------------------------------------------
+#include <Arduino.h>
 #include <ArduinoJson.h>
 JsonDocument jsonCmdReceive;
 JsonDocument jsonInfoSend;
 JsonDocument jsonInfoHttp;
 
-
-//#include <SCServo.h>
-#include <Adafruit_SSD1306.h> // OLED driver
+#include <Adafruit_SSD1306.h>
 #include <INA219_WE.h>
 #include <PID_v2.h>
-#include "battery_ctrl.h" // custom functions to initialize and function INA
-#include "oled_ctrl.h" // Important, because timekeeping is kept here...
-#include "ugv_config.h" // config variables (addresses etc)
-#include "json_cmd.h" // List of JSON commands
+#include "battery_ctrl.h"
+#include "oled_ctrl.h"
+#include "ugv_config.h"
+#include "json_cmd.h"
 #include "motors.h"
-#include "uart_ctrl.h" // Serial bus
-
+#include "uart_ctrl.h"
 #include "config.h"
 #include "i2c_helpers.h"
 #include "qmi8658c.h"
 #include "ak09918c.h"
 #include "madgwick.h"
-#include "motioncal_output.h"
 #include "mag_calibration.h"
 #include "hardcoded_calibration.h"
 
-#define MAIN_LOOP_RATE_HZ     100
-#define MAIN_LOOP_PERIOD_MS   (1000 / MAIN_LOOP_RATE_HZ)  // 10ms
-
-// For debugging: track actual loop rate
-static unsigned long loop_count = 0;
-static unsigned long last_rate_print = 0;
-static float actual_loop_rate = 0.0f;
-
-// IMU streaming timing
-static uint32_t last_imu_stream_ms = 0;
-static float last_dt = 0.01f;
-
-// Sensor instances
+// -----------------------------------------------------------------------------
+// IMU Sensor Instances
+// -----------------------------------------------------------------------------
 QMI8658C imu(QMI8658C_ADDR);
 AK09918C mag(AK09918C_ADDR);
 MadgwickFilter filter(MADGWICK_BETA);
-
-// Calibration receiver
 MagCalibrationReceiver cal_receiver;
 
-// Timing
-uint32_t last_output_ms = 0;
-uint32_t last_update_us = 0;
-
-// Sensor status
+// -----------------------------------------------------------------------------
+// IMU State
+// -----------------------------------------------------------------------------
 bool imu_ok = false;
 bool mag_ok = false;
+uint32_t last_update_us = 0;
 
-// Send IMU stream data as JSON when streaming is enabled
-void sendIMUStreamData() {
-    if (!imu_stream_enabled) return;
+// -----------------------------------------------------------------------------
+// Loop Statistics (for debugging)
+// -----------------------------------------------------------------------------
+static unsigned long loop_count = 0;
 
-    uint32_t now_ms = millis();
-    if (now_ms - last_imu_stream_ms < OUTPUT_INTERVAL_MS) return;
-    last_imu_stream_ms = now_ms;
+// -----------------------------------------------------------------------------
+// IMU Streaming (must be after IMU instances are defined)
+// -----------------------------------------------------------------------------
+#include "imu_stream.h"
 
-    // Get sensor data
-    const QMI8658C_Data& imu_data = imu.get_data();
-    const AK09918C_Data& mag_data = mag.get_data();
-
-    // Build JSON response
-    jsonInfoSend.clear();
-    jsonInfoSend["T"] = FEEDBACK_IMU_STREAM;
-
-    // Accelerometer (g)
-    jsonInfoSend["ax"] = imu_data.accel[0];
-    jsonInfoSend["ay"] = imu_data.accel[1];
-    jsonInfoSend["az"] = imu_data.accel[2];
-
-    // Gyroscope (rad/s)
-    jsonInfoSend["gx"] = imu_data.gyro[0];
-    jsonInfoSend["gy"] = imu_data.gyro[1];
-    jsonInfoSend["gz"] = imu_data.gyro[2];
-
-    // Magnetometer (uT, calibrated)
-    jsonInfoSend["mx"] = mag_data.mag[0];
-    jsonInfoSend["my"] = mag_data.mag[1];
-    jsonInfoSend["mz"] = mag_data.mag[2];
-
-    // Orientation (degrees)
-    jsonInfoSend["pitch"] = filter.get_pitch();
-    jsonInfoSend["roll"] = filter.get_roll();
-    jsonInfoSend["yaw"] = filter.get_yaw();
-
-    // Delta time
-    jsonInfoSend["dt"] = last_dt;
-
-    serializeJson(jsonInfoSend, Serial);
-    Serial.println();
-}
+// =============================================================================
+// Setup
+// =============================================================================
 
 void setup() {
+    // --- Serial ---
     Serial.begin(SERIAL_BAUD);
-    i2c_init(I2C_SDA, I2C_SCL, I2C_FREQ);
-    //Wire.begin(S_SDA, S_SCL);
-    while(!Serial) {}
+    while (!Serial) {}
     delay(1000);
 
-    // Initialize IMU (accelerometer + gyroscope)
+    // --- I2C Bus ---
+    i2c_init(I2C_SDA, I2C_SCL, I2C_FREQ);
+
+    // --- IMU (QMI8658C) ---
     imu_ok = imu.begin(ACCEL_FS_SEL, GYRO_FS_SEL, ODR_SEL);
     if (!imu_ok) {
         Serial.println("ERROR: QMI8658C init failed!");
     } else {
         Serial.println("QMI8658C OK");
+        imu.calibrate_gyro();
     }
 
-    // Initialize magnetometer
+    // --- Magnetometer (AK09918C) ---
     mag_ok = mag.begin(AK_MODE_CONT_100HZ);
     if (!mag_ok) {
         Serial.println("ERROR: AK09918C init failed!");
     } else {
         Serial.println("AK09918C OK");
-    }
-
-    // Calibrate gyroscope (device must be stationary)
-    if (imu_ok) {
-        imu.calibrate_gyro();
-    }
-
-    if (mag_ok) {
         apply_hardcoded_calibration(mag);
     }
 
-
-
+    // --- Power Monitor (INA219) ---
     ina219_init();
     inaDataUpdate();
+
+    // --- Motor Configuration ---
     mm_settings(mainType, moduleType);
+    motionPinInit();
+    pidControllerInit();
+
+    // --- OLED Display ---
     init_oled();
     screenLine_0 = "Custom Waveshare";
     screenLine_1 = "Rover Driver";
@@ -146,64 +109,25 @@ void setup() {
     screenLine_3 = "Starting...";
     oled_update();
     delay(1000);
-    screenLine_2 = screenLine_3;
-    screenLine_3 = "Initialize 12V-switch ctrl";
-    oled_update();
-    if(InfoPrint == 1){Serial.println("Initialize the pins used for 12V-switch ctrl.");}
-    motionPinInit();
-    pidControllerInit();
 
+    screenLine_2 = screenLine_3;
+    screenLine_3 = "Ready";
+    oled_update();
+
+    if (InfoPrint == 1) {
+        Serial.println("Setup complete.");
+    }
 }
 
+// =============================================================================
+// Main Loop
+// =============================================================================
+
 void loop() {
-  static unsigned long last_loop_time = 0;
-  unsigned long loop_start = millis();
-
-  serialCtrl();
-
-  // Read sensors
-  bool imu_read_ok = imu.read();
-  bool mag_read_ok = mag.read();
-
-  // Update orientation filter
-  if (imu_read_ok) {
-      // Calculate delta time
-      uint32_t now_us = micros();
-      float dt = (now_us - last_update_us) / 1000000.0f;
-      last_update_us = now_us;
-
-      // Sanity check dt
-      if (dt <= 0 || dt > 0.1f) {
-          dt = 0.01f;
-      }
-      last_dt = dt;
-
-      // Get sensor data
-      const QMI8658C_Data& imu_data = imu.get_data();
-      const AK09918C_Data& mag_data = mag.get_data();
-
-      // Update Madgwick filter
-      if (mag_read_ok) {
-          filter.update(
-              imu_data.gyro[0], imu_data.gyro[1], imu_data.gyro[2],
-              imu_data.accel[0], imu_data.accel[1], imu_data.accel[2],
-              mag_data.mag[0], mag_data.mag[1], mag_data.mag[2],
-              dt
-          );
-      } else {
-          // Use IMU-only update when magnetometer data not available
-          filter.update_imu(
-              imu_data.gyro[0], imu_data.gyro[1], imu_data.gyro[2],
-              imu_data.accel[0], imu_data.accel[1], imu_data.accel[2],
-              dt
-          );
-      }
-  }
-
-  // Send IMU stream data if enabled
-  sendIMUStreamData();
-
-  oledInfoUpdate();
-  heartBeatCtrl();
-  loop_count++;
+    serialCtrl();
+    updateIMUFilter();
+    sendIMUStreamData();
+    oledInfoUpdate();
+    heartBeatCtrl();
+    loop_count++;
 }
