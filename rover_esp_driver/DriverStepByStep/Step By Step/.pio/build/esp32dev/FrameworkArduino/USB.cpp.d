fec3754589871d50f35e67efecfacd3c92639241
Eyas Taifour <etaifour@icloud.com>
2026-01-06 09:31:41 +1100

    freeRTOS

---
+++ rover_esp_driver/DriverStepByStep/Step By Step/.pio/build/esp32dev/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ rover_esp_driver/DriverStepByStep/Step By Step/.pio/build/esp32dev/FrameworkArduino/WMath.cpp.d
@@ -0,0 +1,18 @@
+.pio/build/esp32dev/FrameworkArduino/WMath.cpp.o: \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/cores/esp32/WMath.cpp \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-log.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp_rom_sys.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/soc/esp32/include/soc/reset_reasons.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp32/rom/ets_sys.h \
+ /Users/eyast/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log_internal.h
